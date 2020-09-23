762224fc6a57478c700e602f05552ef606b5bef6
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-23 13:58:53 -0400

    Update github action

--- .github/workflows/install-packages.jl
+++ .github/workflows/install-packages.jl
@@ -4,6 +4,7 @@ registry = open("Registry.toml", "r") do io
     TOML.parse(io)
 end
 for (uuid, spec) in registry["packages"]
+    println("Installing $(spec["name"]) in $(spec["path"]) with UUID=$uuid")
     Pkg.add(Pkg.PackageSpec(name=spec["name"], uuid=uuid))
 end
 Pkg.precompile()
